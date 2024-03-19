6cdb2e5d2738ed897f3de16ebe4bb214d2d74f45
Chris Hong <hongchris97@gmail.com>
2024-03-19 05:49:12 -0700

    update image and demo paths

--- app/Project.tsx
+++ app/Project.tsx
@@ -7,9 +7,9 @@ import { ProjectProps } from '@/interface/ProjectProps'
 export default function Project() {
     const [activeTab, setActiveTab] = useState<string>('Web');
     const [displayProjects, setDisplayProjects] = useState<ProjectProps[]>([]);
+    const [filteredProjects, setFilteredProjects] = useState<ProjectProps[]>([]);
     const [isViewMoreClicked, setIsViewMoreClicked] = useState<boolean>(false);
-
-    const initialDisplayCount = 3;
+    const initialDisplayCount = 3
 
     useEffect(() => {
         setIsViewMoreClicked(false);
@@ -17,10 +17,19 @@ export default function Project() {
         const filteredProjects = projectData.filter(project => project.type.toLowerCase() === activeTab.toLowerCase());
         const projectsToShow = isViewMoreClicked ? filteredProjects : filteredProjects.slice(0, initialDisplayCount);
         
+        setFilteredProjects(filteredProjects)
         setDisplayProjects(projectsToShow);
     }, [activeTab, isViewMoreClicked]); 
 
-    const handleViewMore = () => setIsViewMoreClicked(true);
+    const handleViewMore = () => {
+        console.log('Before toggle:', isViewMoreClicked);
+        setIsViewMoreClicked(!isViewMoreClicked);
+        console.log('After toggle:', !isViewMoreClicked); // This will show the intended state, not the updated state, due to the async nature of setState
+    }
+
+    useEffect(() => {
+        console.log('isViewMoreClicked updated:', isViewMoreClicked);
+    }, [isViewMoreClicked]);
 
     const btnBaseStyle = 'border px-4 py-2 text-sm font-medium leading-5 transition-colors duration-150 rounded-lg focus:outline-none';
     const btnActiveStyle = 'border-blue-500 text-blue-700 bg-blue-100';
@@ -41,25 +50,28 @@ export default function Project() {
                     />
                 ))}
             </div>
-            <div className='w-full max-w-4xl grid grid-cols-1 md:grid-cols-2 gap-4'>
-                {displayProjects.map((project, index) => (
+            <div className='w-full max-w-4xl grid gap-4'>
+                {isViewMoreClicked ? filteredProjects.map((project, index) => (
                     <CardTemplate 
                         key={project.id}
-                        // title={project.title}
-                        // subtitle={project.subtitle}
-                        // description={project.description}
+                        isReversed={index % 2 !== 0}
+                        {...project}
+                    />
+                )) : filteredProjects.slice(0, initialDisplayCount).map((project, index) => (
+                    <CardTemplate 
+                        key={project.id}
                         isReversed={index % 2 !== 0}
                         {...project}
                     />
                 ))}
             </div>
-            {projectData.filter(project => activeTab === 'All' || project.type === activeTab).length > initialDisplayCount && displayProjects.length <= initialDisplayCount && (
+            {projectData.filter(project => project.type.toLowerCase() === activeTab.toLowerCase()).length > initialDisplayCount && (
                 <ButtonTemplate 
                     btnStyle="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
                     action={handleViewMore} 
-                    title='View More'
+                    title={!isViewMoreClicked ? 'View More' : 'View Less'}
                 />
-            )}
+            )} 
         </div>
     )
 }
