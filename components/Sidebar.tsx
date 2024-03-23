e98bf278629fa821578544543cd254c3ef66f9b5
Chris Hong <hongchris97@gmail.com>
2024-03-22 22:44:53 -0700

    opens new tab on click of icon

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,11 +7,11 @@ export default function Sidebar() {
         <a href="mailto:honggchriss@gmail.com" className="hover:text-blue-500" aria-label="Email">
             <FaEnvelope size={30} />
         </a>
-        <a href="https://github.com/c1008h" className="hover:text-blue-500" aria-label="GitHub">
+        <a href="https://github.com/c1008h" target='__blank' className="hover:text-blue-500" aria-label="GitHub">
             <FaGithub size={30} />
         </a>
         {/* <FaGithubAlt size={30} /> */}
-        <a href="https://linkedin.com/in/hong-chris/" className="hover:text-blue-500" aria-label="LinkedIn">
+        <a href="https://linkedin.com/in/hong-chris/" target='__blank' className="hover:text-blue-500" aria-label="LinkedIn">
             <FaLinkedin size={30} />
         </a>
     </div>
