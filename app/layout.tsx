f3a1cee4c3e940640ed9c7b0c7972e4f54c3f484
Chris Hong <hongchris97@gmail.com>
2024-08-16 23:05:22 -0700

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
+import Script from 'next/script';
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASURE_ME as String;
 
 export const metadata: Metadata = {
   title: "Chris Hong",
@@ -16,7 +18,42 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+        {MEASUREMENT_ID && (
+          <link
+            rel='preload'
+            href={`https://www.googletagmanager.com/gtag/js?id=${MEASUREMENT_ID}`}
+            as='script'
+            crossOrigin='anonymous'
+          />
+        )}
+      <body className={inter.className}>{children}
+      {MEASUREMENT_ID && (
+          <>
+            <Script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${MEASUREMENT_ID}`}
+              strategy='afterInteractive'
+              crossOrigin='anonymous'
+            />
+            <Script
+              id='google-analytics'
+              strategy='afterInteractive'
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${MEASUREMENT_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+              }}
+              crossOrigin='anonymous'
+            />
+          </>
+        )}
+
+      </body>
     </html>
   );
 }
